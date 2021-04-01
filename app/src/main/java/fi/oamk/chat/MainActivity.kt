8ffbe1fbcaa8084c39093081586005559309cf6c
Loreleï <noiraud.lorelei@gmail.com>
2021-04-01 14:05:55 +0300

    Part 2 update

--- app/src/main/java/fi/oamk/chat/MainActivity.kt
+++ app/src/main/java/fi/oamk/chat/MainActivity.kt
@@ -1,9 +1,15 @@
 package fi.oamk.chat
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -12,9 +18,10 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var message: ArrayList<String>
+    private lateinit var messages: ArrayList<String>
     private lateinit var database: DatabaseReference
     private lateinit var edMessage: EditText
+    private lateinit var roMessageList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +31,17 @@ class MainActivity : AppCompatActivity() {
         /*
         //write a message to the database
         val database = Firebase.database
-        val myRef = database.getReference("message")
+        val myRef = database.getReference("messages")
 
         myRef.setValue("Hello, world!")
         */
 
         //PART 2
         edMessage = findViewById(R.id.messageText)
+        roMessageList = findViewById(R.id.messageList)
+
         database = Firebase.database.reference
-        message = arrayListOf()
+        messages = arrayListOf()
 
         edMessage.setOnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
@@ -40,24 +49,44 @@ class MainActivity : AppCompatActivity() {
                 return@setOnKeyListener true
             }
             return@setOnKeyListener false
-
-        }
-    }
-
-    val messageListener = object: ValueEventListener{
-        override fun onDataChange(snapshot: DataSnapshot){
-
         }
 
-        override fun onCancelled(error: DatabaseError){
+        val messageListener = object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot){
+                if( snapshot.value != null) {
+                    val messagesFromDatabase = (snapshot.value as HashMap<Int, ArrayList<String>>).get("messages")
+                    messages.clear()
+                    messagesFromDatabase?.forEach {
+                        if (it != null) messages.add(it)
+                    }
+                    roMessageList.adapter?.notifyDataSetChanged()
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError){
+                Log.d("Chat",error.toString())
+            }
         }
+
+        database.addValueEventListener(messageListener)
+        roMessageList.layoutManager = LinearLayoutManager(this)
+        roMessageList.adapter = MyAdapter(messages)
+
     }
 
     private fun addMessage() {
         val newMessage = edMessage.text.toString()
-        message.add(newMessage)
-        database.child("message").setValue(message)
+        messages.add(newMessage)
+        database.child("messages").setValue(messages)
         edMessage.setText("")
+        closeKeyBoard()
+    }
+
+    private fun closeKeyBoard(){
+        val view = this.currentFocus
+        if (view != null){
+            val imm= getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
